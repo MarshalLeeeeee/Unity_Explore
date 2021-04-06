0518c80ccf773f62e2c655143ff02861c7bbad98
MarshalLeeeeee <marshallee413lmc@sina.com>
2021-04-06 09:55:22 +0200

    Smooth acceleration of forward and rotation with maxSpeed
    Smooth acceleration of forward and rotation with maxSpeed

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    public float forwardSpeed;
+    public float forwardSpeedMax;
+    public float forwardAcceleration;
+    private float forwardSpeed;
     public float turnSpeed;
+
+    private void Start()
+    {
+        forwardSpeed = 0.0f;
+    }
 
     // Update is called once per frame
     void Update()
@@ -13,7 +20,23 @@ public class CarController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float forwardInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed * forwardInput);
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput * forwardInput);
+        if (forwardInput != 0.0f) forwardSpeed += Time.deltaTime * forwardAcceleration * forwardInput;
+        else
+        {
+            if (forwardSpeed > 0.0f)
+            {
+                forwardSpeed -= 0.5f * Time.deltaTime * forwardAcceleration;
+                forwardSpeed = forwardSpeed > 0.0f ? forwardSpeed : 0.0f;
+            }
+            else if (forwardSpeed < 0.0f)
+            {
+                forwardSpeed += 0.5f * Time.deltaTime * forwardAcceleration;
+                forwardSpeed = forwardSpeed < 0.0f ? forwardSpeed : 0.0f;
+            }
+        }
+        forwardSpeed = Mathf.Clamp(forwardSpeed, -forwardSpeedMax, forwardSpeedMax);
+
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * forwardSpeed * horizontalInput);
     }
 }
