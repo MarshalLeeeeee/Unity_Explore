7db773ef7004ab6a6475d0c8d225012938e78bbf
MarshalLeeeeee <marshallee413lmc@sina.com>
2021-04-25 12:14:22 +0200

    init GameScene and impl shoot and reload
    init GameScene and impl shoot and reload

--- Assets/Scripts/WarriorController.cs
+++ Assets/Scripts/WarriorController.cs
@@ -13,24 +13,36 @@ public class WarriorController : MonoBehaviour
     public float horizonSensitivity = 0.5f;
     public float verticalSensitivity = 0.5f;
     public float surfMinTime = 0.5f;
-    public float shootInterval = 0.5f;
-
-    private bool shootMode = true;
-    private float shootTime = -1.0f;
-    private bool inSemiShoot = false;
-    private Animator warriorAnim;
-    private Rigidbody warriorRb;
+    public float singleShootInterval = 0.5f;
+    public float autoShootInterval = 0.1f;
+    public float reloadTime = 2.667f;
+    public int magSize = 30;
+    
     private float forwardSpeed = 0.0f;
     private float horizonSpeed = 0.0f;
     private float xAngle = 0.0f;
     private float yAngle = 0.0f;
+
     private bool onGround = true;
     private bool inAir = false;
+    private bool shootMode = true;
+    private bool inSemiShoot = false;
+    private bool inReloading = false;
+
     private float forwardSpeedInAir;
     private float horizonSpeedInAir;
+
     private bool jumpTrigger = false;
     private bool surfTrigger = false;
+
     private float surfStart;
+    private float shootStart = -1.0f;
+    private float reloadStart;
+
+    private int currentMagSize;
+
+    private Animator warriorAnim;
+    private Rigidbody warriorRb;
     private Quaternion initSpineRotation;
     private Transform rifleTransform;
     private Transform rightIndexTransform;
@@ -42,6 +54,10 @@ public class WarriorController : MonoBehaviour
         initSpineRotation = transform.Find("Hips/Spine").localRotation;
         rifleTransform = transform.Find("Hips/ArmPosition_Right");
         rightIndexTransform = transform.Find("Hips/Spine/Chest/Shoulder_Right/UpperArm_Right/LowerArm_Right/Hand_Right/Index_Proximal_Right/Index_Intermediate_Right");
+        warriorAnim.SetFloat("singleShootProp", 0.3f);
+        warriorAnim.SetFloat("autoShootProp", 0.3f);
+        warriorAnim.SetFloat("reloadProp", 0.3f);
+        currentMagSize = magSize;
     }
 
     private void FixedUpdate()
@@ -133,18 +149,20 @@ public class WarriorController : MonoBehaviour
         }
 
         // shoot
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetMouseButtonDown(1))
         {
-            if (shootMode) shootMode = false;
-            else shootMode = true;
+            if (shootMode) { shootMode = false; }
+            else { shootMode = true; warriorAnim.SetBool("autoShootFlag", false); }
         }
         if (shootMode)
         {
-            if (Input.GetMouseButtonDown(0) && (Time.time >= shootTime + shootInterval))
+            if (Input.GetMouseButtonDown(0) && (Time.time >= shootStart + singleShootInterval) && !inReloading)
             {
                 Instantiate(bullet, rifleTransform.position - 0.9f * rifleTransform.right - 0.9f * rifleTransform.up + 0.01f * rifleTransform.forward, rifleTransform.rotation * Quaternion.Euler(0.0f,0.0f,135.0f));
-                shootTime = Time.time;
+                currentMagSize -= 1;
+                shootStart = Time.time;
                 inSemiShoot = true;
+                warriorAnim.SetTrigger("singleShootTrigger");
             }
             else if (Input.GetMouseButtonUp(0))
             {
@@ -153,15 +171,32 @@ public class WarriorController : MonoBehaviour
         }
         else
         {
-            if (Input.GetMouseButton(0) && (Time.time >= shootTime + shootInterval) && !inSemiShoot)
+            if (Input.GetMouseButton(0) && (Time.time >= shootStart + autoShootInterval) && !inSemiShoot && !inReloading)
             {
                 Instantiate(bullet, rifleTransform.position - 0.9f * rifleTransform.right - 0.9f * rifleTransform.up + 0.01f * rifleTransform.forward, rifleTransform.rotation * Quaternion.Euler(0.0f, 0.0f, 135.0f));
-                shootTime = Time.time;
+                currentMagSize -= 1;
+                shootStart = Time.time;
+                warriorAnim.SetBool("autoShootFlag", true);
             }
             else if (Input.GetMouseButtonUp(0))
             {
                 inSemiShoot = false;
+                warriorAnim.SetBool("autoShootFlag", false);
             }
+        }
+
+        // reload
+        Debug.Log(currentMagSize);
+        if (((Input.GetKeyDown(KeyCode.R) && currentMagSize < magSize) || (currentMagSize == 0)) && !inReloading)
+        {
+            inReloading = true;
+            reloadStart = Time.time;
+            warriorAnim.SetTrigger("reloadTrigger");
+        }
+        if (inReloading && Time.time > reloadStart + reloadTime)
+        {
+            inReloading = false;
+            currentMagSize = magSize;
         }
     }
 
